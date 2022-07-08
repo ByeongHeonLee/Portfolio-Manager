4defdd2003fbe8b270632521c2813d653cdef052
Byeong Heon Lee <lww7438@gmail.com>
2022-07-08 18:55:33 +0900

    Construct the functionality to search KOSPI

--- Client/client/src/components/views/KosPage/KosPage.js
+++ Client/client/src/components/views/KosPage/KosPage.js
@@ -3,21 +3,29 @@
 import { React, useState } from "react";
 import TextField from "@mui/material/TextField";
 import List from "./Components/KosPage"
-import "./App.css";
+import "./KosPage.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
     <div className="main">
-      <h1>KOSPI Search</h1>
+      <h1>React Search</h1>
       <div className="search">
         <TextField
           id="outlined-basic"
+          onChange={inputHandler}
           variant="outlined"
           fullWidth
           label="Search"
         />
       </div>
-      <List />
+      <List input={inputText} />
     </div>
   );
 }
