5ab1a699b9bec28ee53b6b7c11799c52320805c4
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-09-01 17:37:12 +0900

    Update LoginPage.js

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -9,7 +9,7 @@ import { useDispatch } from "react-redux";
 import './Loginbtn.css';
 
 const { Title } = Typography;
-
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
 function LoginPage(props) {
   const dispatch = useDispatch();
   const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
