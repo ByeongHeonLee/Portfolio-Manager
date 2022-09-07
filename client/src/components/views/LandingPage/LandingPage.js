f8b2ba9a9a43e1099198ff4525bc0b58f0df5100
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-09-07 13:21:55 +0900

    Update LandingPage.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Button, Icon } from "antd";
-import "./LandingPage.css";
 
 import Search from "../KosPage/Search";
 import financials_kr from "../KosPage/data/financials_kr.json";
