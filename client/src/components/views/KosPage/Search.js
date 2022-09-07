8b556eb73b6e8392da4a9e1b34f8786e45fc3ea3
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-09-07 14:21:55 +0900

    Update Search.js

--- client/src/components/views/KosPage/Search.js
+++ client/src/components/views/KosPage/Search.js
@@ -43,7 +43,12 @@ function Search({ details }) {
           placeholder = "종목명을 입력하세요" 
           onChange = {handleChange}
         />
-        <Icon className="searchButton" type="search" style={{ color: 'rgba(00,B4,CC,.25)' }} />
+        <Button type="primary" class="searchButton" href="/kos"
+            style={{ width: '80px', height: '80px', border: '1px solid #000000', textAlign: 'center',
+               color: 'rgba(00,B4,CC,.25)', backgroundColor: '#000000', borderRadius: '0 5px 5px 0', cursor: 'pointer', fontSize: '30px'}}>
+            <Icon type='search' style={{ padding: '23px 0 0 0' }}/>
+        </Button> 
+        //<Icon className="searchButton" type="search" style={{ color: 'rgba(00,B4,CC,.25)' }} />
       </div>
       {searchList()}
     </section>
