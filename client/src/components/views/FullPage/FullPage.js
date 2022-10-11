a49f6e00f7dc5e8d68ef9dba40411f4512d91af8
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-10-12 01:45:48 +0900

    Update FullPage.js

--- client/src/components/views/FullPage/FullPage.js
+++ client/src/components/views/FullPage/FullPage.js
@@ -3,7 +3,9 @@ import { FullPage, Slide } from 'react-full-page';
 import { Icon, Button } from 'antd';
 import './FullPage.css'
 import man_img from '../../../images/character.png'
-import searchbar_img from '../../../images/search_bar.png'
+import kospi_img from '../../../images/kospi_img.png'
+import kosdaq_img from '../../../images/kosdaq_img.png'
+import krx_img from '../../../images/krx_img.png'
 
 export default class fullPage extends React.Component {
   render() {
@@ -29,8 +31,11 @@ export default class fullPage extends React.Component {
               </Slide>
               <Slide>
               <div className="section-common section-area3">
+                <img src={kospi_img} className='kospi_img'></img>
+                <img src={kosdaq_img} className='kosdaq_img'></img>
+                <img src={krx_img} className='krx_img'></img>
                 <Button type="primary" size="large" href="/index" 
-                  style={{ border: '1px solid rgb(0, 88, 202)', backgroundColor:'rgb(0, 88, 202)', color: 'white', top: '75%', left: '70%',
+                  style={{ border: '1px solid rgb(0, 88, 202)', backgroundColor:'rgb(0, 88, 202)', color: 'white', top: '75%',
                   borderRadius: '20px', padding: '8px', height:'60px', width: '200px', fontSize: '20px', fontWeight: 'bolder' }}>
                     지수 확인하기</Button>
               </div>
